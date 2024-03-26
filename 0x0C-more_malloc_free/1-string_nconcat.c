ff156415fe2e76251bd90bbd80385d2e06577c9f
Ranzome <enebiransom@gmail.com>
2024-03-26 05:20:15 -0700

    move

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-	for (x = 0; s1[x]; ++x);
+	for (x = 0; s1[x];
+		       	++x);
 	}
 	if (s2 == NULL)
 	{
@@ -30,17 +31,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-	for (y = 0; s2[y]; ++y);
+	for (y = 0; s2[y]; 
+			++y);
 	}
 	if (y > n)
 	y = n;
 	s = malloc(sizeof(char) * (x + y + 1));
 	if (s == NULL)
 	return (NULL);
-	for (z = 0; z < x; 
+	for (z = 0; z < x;
 			z++)
 	s[z] = s1[z];
-	for (z = 0; z < y; 
+	for (z = 0; z < y
 			z++)
 	s[z + x] = s2[z];
 	s[x + y] = '\0';
